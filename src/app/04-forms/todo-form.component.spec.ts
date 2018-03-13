c1e84352d733373013697d3ff3866b780ec2c42f
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-03-13 20:56:25 -0300

    .

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,22 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
+
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  var component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('', () => {
+  it('should make the name control is required', () => {
+    let control = component.form.get('name');
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
   });
 });
--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -7,6 +7,13 @@ describe('VoteComponent', () => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    let totalVotes = null;
+        component.voteChanged.subscribe(tv => totalVotes = tv);
+
+        component.upVote();
+
+       // expect(totalVotes).not.toBeNull();
+       expect(totalVotes).toBe(1);
   });
 });
