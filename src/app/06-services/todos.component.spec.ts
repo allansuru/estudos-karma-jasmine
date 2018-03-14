cdb30024eb81f9895f6694dafd0f9f9124a53af2
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-03-14 20:50:32 -0300

    test unit if api

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,26 @@
+
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+import { CompilePipeMetadata } from '@angular/compiler';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the itens returned from the server', () => {
+    let todos = [1, 2, 3];
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([ todos ]);
+    });
+    component.ngOnInit();
+
+    expect(component.todos).toBe(todos);
   });
 });
