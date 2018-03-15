8c12375b04e4a4257a53cac2551e1fe6d6e6902a
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-03-15 20:58:50 -0300

    unit test ti add, error and added

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,8 +1,10 @@
+import { AppModule } from './../app.module';
 
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
 import { CompilePipeMetadata } from '@angular/compiler';
 
 describe('TodosComponent', () => {
@@ -19,8 +21,28 @@ describe('TodosComponent', () => {
     spyOn(service, 'getTodos').and.callFake(() => {
       return Observable.from([ todos ]);
     });
+
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
   });
+
+  it('should the call the server to save the changes when a new todo item is added', () => {
+   let spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty()
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    let todo = {id: 1};
+    let spy = spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
+    
+     component.add();
+ 
+     expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+   });
 });
