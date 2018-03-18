d5e5e0372f8ead8d6d91e43bdaabdfb1bc532ee7
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-03-18 17:53:42 -0300

    unit test delete + win confirm

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -5,6 +5,7 @@ import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 import { CompilePipeMetadata } from '@angular/compiler';
 
 describe('TodosComponent', () => {
@@ -40,9 +41,38 @@ describe('TodosComponent', () => {
   it('should add the new todo returned from the server', () => {
     let todo = {id: 1};
     let spy = spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
-    
+
      component.add();
  
      expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+   });
+
+   it('should set the message property if server return error when adding a new todo', () => {
+    let error = 'error from the server';
+    let spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+    
+     component.add();
+
+     expect(component.message).toBe(error);
+
+   });
+
+   it('should call the server to delete a todo item if the use confirms', () => {
+    spyOn(window,'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty())
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
+   });
+
+
+   it('should NOT call the server to delete a todo item if the use cancels', () => {
+    spyOn(window,'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty())
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalledWith(1);
    });
 });
